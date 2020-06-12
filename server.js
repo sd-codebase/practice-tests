616f78a5fcbfb239cc401663925247c3751bcaa6
Shrikrishna Dhas <shrikrishna.dhas@simplifai.ai>
2020-06-12 12:10:29 +0530

    feat: deploy

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const forceSSL = function() {
+    return function(req, res, next) {
+        if (req.headers['x-forwarded-proto'] !== 'https') {
+            return res.redirect(
+                ['https://', req.get('host'), req.url].join('');
+            );
+        }
+        next();
+    }
+};
+
+app.use(express.static(__dirname + '/dist/online-quiz-app'));
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/online-quiz-app/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
